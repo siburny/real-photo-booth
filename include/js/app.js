b1be22a7dd88faf13e53d6bd930695e1df4f9b9a
Maxim Rubis <siburny@gmail.com>
2018-11-30 08:31:00 -0500

    Take 3 frames; convert to Promise

--- include/js/app.js
+++ include/js/app.js
@@ -5,11 +5,8 @@ const config = require('./config');
 const path = require('path');
 const fs = require('fs');
 
-function delay(ms) {
-  return function (x) {
-    return new Promise(resolve => setTimeout(() => resolve(x), ms));
-  };
-}
+const delay = (t, v) => new Promise(resolve => setTimeout(resolve.bind(null, v), t));
+Promise.prototype.delay = delay;
 
 class App {
   constructor() {
@@ -40,35 +37,62 @@ class App {
 
     $('#step1,#step2,#step3').hide();
     $('#app').show();
-    $('#step1').addClass('fadein').show();
 
     $('#step1 #text1, #step1 #text2, #step1 #text3, #step1 #text4, #step1 #text5').hide();
+    $('#step1').addClass('fadein').show();
 
+    const frames = [];
+    this.captureFrame(1)
+      .then((filename) => {
+        frames.push(filename);
+        return this.captureFrame(2);
+      })
+      .then((filename) => {
+        frames.push(filename);
+        return this.captureFrame(3);
+      })
+      .then((filename) => {
+        frames.push(filename);
+      });
+  }
+
+  captureFrame(i) {
+    $('#frameNumber').text('Frame #' + i);
     $('#step1 #text1').addClass('fadein').show();
 
-    setTimeout(() => {
-      $('#step1 #text1').removeClass('fadein').hide();
-      $('#step1 #text2').addClass('enlarge').show();
-    }, 3000);
-    setTimeout(() => {
-      $('#step1 #text2').removeClass('enlarge').hide();
-      $('#step1 #text3').addClass('enlarge').show();
-    }, 4000);
-    setTimeout(() => {
-      $('#step1 #text3').removeClass('enlarge').hide();
-      $('#step1 #text4').addClass('enlarge').show();
-    }, 5000);
-    setTimeout(() => {
-      $('#step1 #text4').removeClass('enlarge').hide();
-      $('#step1 #text5').addClass('fadein').show();
+    return delay(3000)
+      .then(() => {
+        $('#step1 #text1').removeClass('fadein').hide();
+        $('#step1 #text2').addClass('enlarge').show();
 
-      ipc.on('camera-capture-done', function (event, arg) {
-        alert(arg);
+        return delay(1000);
+      })
+      .then(() => {
+        $('#step1 #text2').removeClass('enlarge').hide();
+        $('#step1 #text3').addClass('enlarge').show();
+
+        return delay(1000);
+      })
+      .then(() => {
+        $('#step1 #text3').removeClass('enlarge').hide();
+        $('#step1 #text4').addClass('enlarge').show();
+
+        return delay(1000);
+      })
+      .then(() => {
+        $('#step1 #text4').removeClass('enlarge').hide();
+        $('#step1 #text5').addClass('fadein').show();
+
+        ipc.on('camera-capture-done', function (event, arg) {
+          $('#step1 #text5').removeClass('fadein').hide();
+          if (arg) {
+            return arg;
+          }
+
+          throw new Error('Empty file name');
+        });
+        ipc.send('camera-capture', path.join(this.path, 'frame' + i));
       });
-
-      console.log('Capture frame #1');
-      ipc.send('camera-capture', path.join(this.path, 'frame1'));
-    }, 5000);
   }
 }
 
